4f99c39bc63f6908e52d6080846011bd48843c7c
TeodorK0907 <ted_kehayov@abv.bg>
2024-04-12 16:17:58 +0300

    first commit

---
+++ src/test/java/org/example/currencyconverterapi/CurrencyConverterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.currencyconverterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
