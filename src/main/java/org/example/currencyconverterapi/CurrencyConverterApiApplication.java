7e245642b5805d5e19886a2c301bd0e0656fae09
TeodorK0907 <ted_kehayov@abv.bg>
2024-04-14 20:36:55 +0300

    Update CurrencyConverterApiApplication

--- src/main/java/org/example/currencyconverterapi/CurrencyConverterApiApplication.java
+++ src/main/java/org/example/currencyconverterapi/CurrencyConverterApiApplication.java
@@ -1,9 +1,12 @@
 package org.example.currencyconverterapi;
 
+import org.example.currencyconverterapi.configurations.CurrencyBeaconConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CurrencyBeaconConfigProperties.class)
 public class CurrencyConverterApiApplication {
 
     public static void main(String[] args) {
