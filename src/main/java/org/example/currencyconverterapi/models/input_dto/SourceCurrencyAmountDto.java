7f93537aa8238ccfcb3fa61c3e324a3eedd1f25f
TeodorK0907 <ted_kehayov@abv.bg>
2024-04-17 23:16:08 +0300

    Update ConversionOutputDto and SourceCurrencyAmountDto

--- src/main/java/org/example/currencyconverterapi/models/input_dto/SourceCurrencyAmountDto.java
+++ src/main/java/org/example/currencyconverterapi/models/input_dto/SourceCurrencyAmountDto.java
@@ -1,4 +1,4 @@
-package org.example.currencyconverterapi.models;
+package org.example.currencyconverterapi.models.input_dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
