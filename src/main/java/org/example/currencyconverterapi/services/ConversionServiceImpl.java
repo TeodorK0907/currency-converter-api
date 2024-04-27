0b41e31b6a74fc174dfe7f89c63a0d81d42be4ee
TeodorK0907 <ted_kehayov@abv.bg>
2024-04-27 12:32:26 +0300

    Update ConversionServiceImpl

--- src/main/java/org/example/currencyconverterapi/services/ConversionServiceImpl.java
+++ src/main/java/org/example/currencyconverterapi/services/ConversionServiceImpl.java
@@ -1,18 +1,16 @@
 package org.example.currencyconverterapi.services;
 
-import org.example.currencyconverterapi.configurations.WebClientConfig;
+import org.example.currencyconverterapi.clients.contracts.CurrencyBeaconClient;
 import org.example.currencyconverterapi.exceptions.UnsuccessfulResponseException;
-import org.example.currencyconverterapi.helpers.ConversionFilterOptionsValidator;
+import org.example.currencyconverterapi.helpers.filter_options_helper.ConversionFilterOptionsValidator;
 import org.example.currencyconverterapi.models.Conversion;
 import org.example.currencyconverterapi.models.input_dto.ConversionFilterOptions;
 import org.example.currencyconverterapi.repositories.ConversionRepository;
 import org.example.currencyconverterapi.services.contracts.ConversionService;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 
 import java.math.BigDecimal;
@@ -23,37 +21,27 @@ import static org.example.currencyconverterapi.helpers.ConstantHelper.*;
 
 @Service
 public class ConversionServiceImpl implements ConversionService {
-    private final String API_KEY_REQUEST_PARAM = "api_key";
-    private final String BASE_CURRENCY_REQUEST_PARAM = "base";
-    private final String FROM_REQUEST_PARAM = "from";
-    private final String TO_REQUEST_PARAM = "to";
-    private final String AMOUNT_REQUEST_PARAM = "amount";
+
     private final String RESPONSE_KEY_VALUE = "value";
     private final String RESPONSE_KEY_RATES = "rates";
     private final String RESPONSE_KEY_META = "meta";
     private final String RESPONSE_KEY_RESPONSE = "response";
     private final String RESPONSE_KEY_CODE = "code";
-    private final String EXCHANGE_RATE_ENDPOINT = "/latest";
-    private final String CONVERT_ENDPOINT = "/convert";
-
-    @Value("${currency.beacon.api-key}")
-    private String key;
 
     private final ConversionRepository conversionRepository;
-
-    private final WebClientConfig webClient;
+    private final CurrencyBeaconClient client;
 
     @Autowired
     public ConversionServiceImpl(ConversionRepository conversionRepository,
-                                 WebClientConfig webClient) {
+                                 CurrencyBeaconClient client) {
         this.conversionRepository = conversionRepository;
-        this.webClient = webClient;
+        this.client = client;
     }
 
     @Override
     public double getExchangeRate(Currency source, Currency target) {
         JSONObject responseJSON = new JSONObject
-                (getExchangeRateResponse(source.getCurrencyCode()));
+                (client.getExchangeRateResponse(source.getCurrencyCode()));
         JSONObject responseCode = responseJSON.getJSONObject(RESPONSE_KEY_META);
         if (responseCode.getInt(RESPONSE_KEY_CODE) == CURRENCY_BEACON_SUCCESS) {
             JSONObject response = responseJSON.getJSONObject(RESPONSE_KEY_RESPONSE);
@@ -67,7 +55,7 @@ public class ConversionServiceImpl implements ConversionService {
     @Override
     public void createConversionAmount(Conversion conversion, BigDecimal amount) {
         JSONObject responseJSON = new JSONObject
-                (getConversionResponse(conversion, amount));
+                (client.getConversion(conversion, amount));
         JSONObject responseCode = responseJSON.getJSONObject(RESPONSE_KEY_META);
         if (responseCode.getInt(RESPONSE_KEY_CODE) == CURRENCY_BEACON_SUCCESS) {
             JSONObject response = responseJSON.getJSONObject(RESPONSE_KEY_RESPONSE);
@@ -113,33 +101,5 @@ public class ConversionServiceImpl implements ConversionService {
     private void processConversion(Conversion conversion, JSONObject response) {
         conversion.setTimeStamp(LocalDateTime.now());
         conversion.setAmount(BigDecimal.valueOf(response.getDouble(RESPONSE_KEY_VALUE)));
-    }
-
-    private String getConversionResponse(Conversion conversion, BigDecimal amount) {
-
-        return webClient.webClient().get()
-                .uri(uriBuilder -> uriBuilder
-                        .path(CONVERT_ENDPOINT)
-                        .queryParam(API_KEY_REQUEST_PARAM, key)
-                        .queryParam(FROM_REQUEST_PARAM, conversion.getSourceCurrency())
-                        .queryParam(TO_REQUEST_PARAM, conversion.getTargetCurrency())
-                        .queryParam(AMOUNT_REQUEST_PARAM, amount.toBigInteger())
-                        .build())
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
-    }
-
-    private String getExchangeRateResponse(String sourceCurrencyCode) {
-        WebClient client = webClient.webClient();
-        return client.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path(EXCHANGE_RATE_ENDPOINT)
-                        .queryParam(API_KEY_REQUEST_PARAM, key)
-                        .queryParam(BASE_CURRENCY_REQUEST_PARAM, sourceCurrencyCode)
-                        .build())
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
     }
 }
