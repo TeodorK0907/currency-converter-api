7c6ea74aee79b784673e96c463f4e502c14a08e3
TeodorK0907 <ted_kehayov@abv.bg>
2024-04-29 19:50:25 +0300

    Update ConversionService and ConversionServiceImpl

--- src/main/java/org/example/currencyconverterapi/services/ConversionServiceImpl.java
+++ src/main/java/org/example/currencyconverterapi/services/ConversionServiceImpl.java
@@ -2,9 +2,11 @@ package org.example.currencyconverterapi.services;
 
 import org.example.currencyconverterapi.clients.contracts.CurrencyBeaconClient;
 import org.example.currencyconverterapi.exceptions.UnsuccessfulResponseException;
+import org.example.currencyconverterapi.helpers.ExchangeRateCacheManager;
 import org.example.currencyconverterapi.helpers.filter_options_helper.ConversionFilterOptionsValidator;
 import org.example.currencyconverterapi.models.Conversion;
 import org.example.currencyconverterapi.models.input_dto.ConversionFilterOptions;
+import org.example.currencyconverterapi.models.input_dto.CurrencyPairDto;
 import org.example.currencyconverterapi.repositories.ConversionRepository;
 import org.example.currencyconverterapi.services.contracts.ConversionService;
 import org.json.JSONException;
@@ -16,7 +18,6 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.*;
 
 import static org.example.currencyconverterapi.helpers.ConstantHelper.*;
 
@@ -31,27 +32,35 @@ public class ConversionServiceImpl implements ConversionService {
 
     private final ConversionRepository conversionRepository;
     private final CurrencyBeaconClient client;
+    private final ExchangeRateCacheManager cacheManager;
 
     @Autowired
     public ConversionServiceImpl(ConversionRepository conversionRepository,
-                                 CurrencyBeaconClient client) {
+                                 CurrencyBeaconClient client,
+                                 ExchangeRateCacheManager cacheManager) {
         this.conversionRepository = conversionRepository;
         this.client = client;
+        this.cacheManager = cacheManager;
     }
 
     @Override
-    public double getExchangeRate(Currency source, Currency target) {
-        JSONObject responseJSON = new JSONObject
-                (client.getExchangeRateResponse(source.getCurrencyCode()));
-        JSONObject responseCode = responseJSON.getJSONObject(RESPONSE_KEY_META);
-        if (responseCode.getInt(RESPONSE_KEY_CODE) == CURRENCY_BEACON_SUCCESS) {
-            JSONObject response = responseJSON.getJSONObject(RESPONSE_KEY_RESPONSE);
-            JSONObject exchangeRateJson = response.getJSONObject(RESPONSE_KEY_RATES);
-            return exchangeRateJson.getDouble(target.getCurrencyCode());
+    public double getExchangeRate(CurrencyPairDto pair) {
+        if (cacheManager.contains(pair)) {
+            return cacheManager.get(pair);
+        } else {
+            JSONObject responseJSON = new JSONObject
+                    (client.getExchangeRateResponse(pair.getSource().getCurrencyCode()));
+            JSONObject responseCode = responseJSON.getJSONObject(RESPONSE_KEY_META);
+            if (responseCode.getInt(RESPONSE_KEY_CODE) == CURRENCY_BEACON_SUCCESS) {
+                JSONObject response = responseJSON.getJSONObject(RESPONSE_KEY_RESPONSE);
+                JSONObject exchangeRateJson = response.getJSONObject(RESPONSE_KEY_RATES);
+                double exchangeRate = exchangeRateJson.getDouble(pair.getTarget().getCurrencyCode());
+                cacheManager.put(pair, exchangeRate);
+                return exchangeRate;
+            }
         }
         throw new UnsuccessfulResponseException(COULD_NOT_PROCESS_REQUEST_ERROR_MESSAGE);
     }
-
 
     @Override
     public void createConversionAmount(Conversion conversion, BigDecimal amount) {
